41064c75a472f0b4d8a33aba338ca27f452ee871
davariijs <Atuor127@gmail.com>
2025-02-11 21:33:32 -0500

    create model

---
+++ frontend/src/models/Habit.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, Document, Model } from 'mongoose';
+
+export interface IHabit extends Document {
+  userId: string; // Add userId
+  name: string;
+  color: string;
+  question: string;
+  frequencyType: 'everyday' | 'everyXDays' | 'XTimesPerWeek' | 'XTimesPerMonth' | 'XTimesInXDays';
+  frequencyValue: number;
+  frequencyValue2?: number;
+  reminderTime?: string;
+  startDate: Date;
+  completions: { date: Date; completed: boolean }[];
+}
+
+const HabitSchema: Schema = new Schema({
+  userId: { type: String, required: true }, // Add userId field
+  name: { type: String, required: true },
+  color: { type: String, required: true },
+  question: { type: String, required: true },
+  frequencyType: {
+    type: String,
+    required: true,
+    enum: ['everyday', 'everyXDays', 'XTimesPerWeek', 'XTimesPerMonth', 'XTimesInXDays'],
+  },
+  frequencyValue: { type: Number, required: true },
+  frequencyValue2: { type: Number },
+  reminderTime: { type: String },
+  startDate: { type: Date, default: Date.now },
+  completions: [{
+    date: { type: Date },
+    completed: { type: Boolean, default: false },
+  }],
+});
+
+const HabitModel: Model<IHabit> = mongoose.models.Habit || mongoose.model<IHabit>('Habit', HabitSchema);
+
+export default HabitModel;
