f865cb5f6118e68b95f8cf466d5822feb7b18b21
davariijs <Atuor127@gmail.com>
2025-02-14 13:32:43 -0500

    fix google login

--- frontend/src/features/auth/components/google-auth-button.tsx
+++ frontend/src/features/auth/components/google-auth-button.tsx
@@ -3,22 +3,23 @@
 import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
-import { Icons } from '@/components/icons';
 import GoogleIcon from '@mui/icons-material/Google';
-import { IconButton } from '@mui/material';
 
 export default function GoogleSignInButton() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
+
+  const handleGoogleSignIn = () => {
+    console.log('Callback URL:', callbackUrl);
+    signIn('google', { callbackUrl: callbackUrl ?? '/dashboard' });
+  };
 
   return (
     <Button
       className='w-full'
       variant='outline'
       type='button'
-      onClick={() =>
-        signIn('google', { callbackUrl: callbackUrl ?? '/dashboard' })
-      }
+      onClick={handleGoogleSignIn}
     >
       <GoogleIcon sx={{width:'16px', height:'16px'}}/>
       <div className='ml-2'>Continue with Google</div>
