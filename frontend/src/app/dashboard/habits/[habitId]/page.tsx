c05e577b842fe6c26236d9c03157dd7506c1907d
davariijs <Atuor127@gmail.com>
2025-02-20 15:33:52 -0500

    add color and add metadata

--- frontend/src/app/dashboard/habits/[habitId]/page.tsx
+++ frontend/src/app/dashboard/habits/[habitId]/page.tsx
@@ -5,15 +5,25 @@ import HabitCharts from '@/components/chartsProgress/HabitCharts';
 
 interface HabitPageProps {
     params: { habitId: string };
+    searchParams: { color?: string };
   }
 
-const HabitPage = ({ params }: HabitPageProps) => {
+  export const metadata = {
+    title: 'Dashboard : Habit'
+  };
+  
+  
+
+const HabitPage = ({ params, searchParams }: HabitPageProps) => {
     if (!params.habitId) return notFound();
+
+    const habitColor = searchParams.color || '#8884d8'; // Default color if not provided
+
   return (
     <div>
       <h2>Habit Progress</h2>
       <Suspense fallback={<p>Loading...</p>}>
-        <HabitCharts habitId={params.habitId} />
+        <HabitCharts habitId={params.habitId} habitColor={habitColor}/>
       </Suspense>
     </div>
   );
