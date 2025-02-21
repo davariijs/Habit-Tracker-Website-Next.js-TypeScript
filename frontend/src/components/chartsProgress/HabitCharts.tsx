aaa9641057b8d303ee71292a12e0bc6ea37407c4
davariijs <Atuor127@gmail.com>
2025-02-21 07:00:28 -0500

    fix title and select

--- frontend/src/components/chartsProgress/HabitCharts.tsx
+++ frontend/src/components/chartsProgress/HabitCharts.tsx
@@ -3,6 +3,15 @@
 import { useEffect, useState } from 'react';
 import HabitHistoryChart from './HabitHistoryChart';
 import HabitScoreChart from './HabitScoreChart';
+import { Heading } from '../ui/heading';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+
 
 interface HabitDetailProps {
   habitId: string;
@@ -13,7 +22,7 @@ const HabitCharts: React.FC<HabitDetailProps> = ({ habitId,habitColor}) => {
   const [habit, setHabit] = useState<any>(null);
   const [loading, setLoading] = useState(true);
   const [range, setRange] = useState<'day' | 'week' | 'month' | 'year'>('week');
-
+  const rangeOptions = ['day', 'week', 'month', 'year'];
   useEffect(() => {
     if (!habitId) return;
 
@@ -38,13 +47,34 @@ const HabitCharts: React.FC<HabitDetailProps> = ({ habitId,habitColor}) => {
   if (!habit) return <p>Habit not found</p>;
 
   return (
-    <div>
-      <select onChange={(e) => setRange(e.target.value as 'day' | 'week' | 'month' | 'year')} value={range}>
-        <option value="day">Day</option>
-        <option value="week">Week</option>
-        <option value="month">Month</option>
-        <option value="year">Year</option>
-      </select>
+    <div className='flex flex-1 flex-col space-y-5 px-6'>
+      <div className='flex items-start justify-between py-3'>
+            <Heading
+            title='Habit Progress'
+            description=''
+            />
+      </div>
+
+      <div className='flex items-center space-x-2'>
+              <p className='whitespace-nowrap text-sm font-medium'>
+                Show by
+              </p>
+              <Select
+                value={range}
+                onValueChange={(value) => setRange(value as 'day' | 'week' | 'month' | 'year')}
+              >
+                <SelectTrigger className='h-8 w-[150px]' style={{color:habitColor}}>
+                <SelectValue>{range}</SelectValue>
+                </SelectTrigger>
+                <SelectContent side='top'>
+                  {rangeOptions.map((option) => (
+                    <SelectItem key={option} value={option}>
+                      {option}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+        </div>
 
       <HabitScoreChart habitId={habitId} range={range} habitColor={habitColor}/>
       {range !== 'day' && <HabitHistoryChart habitId={habitId} range={range as 'week' | 'month' | 'year'} habitColor={habitColor}/>}
