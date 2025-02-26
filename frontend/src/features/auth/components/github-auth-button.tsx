86e81f922af0342fb0f5a8479871861d5ebf4aac
davariijs <Atuor127@gmail.com>
2025-02-26 14:15:45 -0500

    add loading

--- frontend/src/features/auth/components/github-auth-button.tsx
+++ frontend/src/features/auth/components/github-auth-button.tsx
@@ -4,22 +4,40 @@ import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import { Icons } from '@/components/icons';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
 
 export default function GithubSignInButton() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
-
+  const [loading, setLoading] = useState<boolean>(false);
+  
   return (
     <Button
       className='w-full'
       variant='outline'
       type='button'
-      onClick={() =>
-        signIn('github', { callbackUrl: callbackUrl ?? '/dashboard' })
-      }
+      onClick={async () => {
+        try {
+          setLoading(true);
+          await signIn('github', { 
+            callbackUrl: callbackUrl ?? '/dashboard'
+          });
+        } catch (error) {
+          console.error('GitHub sign-in error:', error);
+          setLoading(false);
+        }
+      }}
     >
-      <Icons.gitHub className='mr-2 h-4 w-4' />
-      Continue with Github
+      {loading ? (
+        <CircularProgress size={20} color="inherit" />
+        ) : (
+        <div className='flex'>
+        <Icons.gitHub className='mr-2 h-4 w-4' />
+        Continue with Github
+        </div>
+      )}
+      
     </Button>
   );
 }
--- frontend/src/features/auth/components/google-auth-button.tsx
+++ frontend/src/features/auth/components/google-auth-button.tsx
@@ -4,25 +4,40 @@ import { useSearchParams } from 'next/navigation';
 import { signIn } from 'next-auth/react';
 import { Button } from '@/components/ui/button';
 import GoogleIcon from '@mui/icons-material/Google';
+import { useState } from 'react';
+import { CircularProgress } from '@mui/material';
 
 export default function GoogleSignInButton() {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams.get('callbackUrl');
-
-  const handleGoogleSignIn = () => {
-    console.log('Callback URL:', callbackUrl);
-    signIn('google', { callbackUrl: callbackUrl ?? '/dashboard' });
-  };
+  const [loading, setLoading] = useState<boolean>(false);
 
   return (
     <Button
       className='w-full'
       variant='outline'
       type='button'
-      onClick={handleGoogleSignIn}
+      onClick={async () => {
+        try {
+          setLoading(true);
+          await signIn('google', { 
+            callbackUrl: callbackUrl ?? '/dashboard'
+          });
+        } catch (error) {
+          console.error('Google sign-in error:', error);
+          setLoading(false);
+        }
+      }}
     >
-      <GoogleIcon sx={{width:'16px', height:'16px'}}/>
-      <div className='ml-2'>Continue with Google</div>
+      {loading ? (
+              <CircularProgress size={20} color="inherit" />
+              ) : (
+              <div className='flex'>
+              <GoogleIcon sx={{width:'16px', height:'16px'}}/>
+              <div className='ml-2'>Continue with Google</div>
+              </div>
+            )}
+      
     </Button>
   );
 }
