acf071619879b3d660fa4c2722167fd0292036df
davariijs <Atuor127@gmail.com>
2025-02-28 11:20:26 -0500

    fix next.config for pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const withPWA = require('next-pwa')({
+  dest: 'public', // The service worker and PWA assets will be stored in `public`
+  register: true, // Automatically registers the service worker
+  skipWaiting: true, // Activates new service worker immediately after installation
+  disable: process.env.NODE_ENV === 'development', // Disables PWA in development mode
+});
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   images: {
     remotePatterns: [
       {
@@ -26,6 +32,6 @@ const nextConfig = {
     };
     return config;
   },
-};
+});
 
 module.exports = nextConfig;
