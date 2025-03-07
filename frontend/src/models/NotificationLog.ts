73e9b5559d8e5fd048860b662a1471627f429f53
davariijs <Atuor127@gmail.com>
2025-03-07 12:28:26 -0500

    Create notification schema

---
+++ frontend/src/models/NotificationLog.ts
@@ -0,0 +1,22 @@
+// In your models folder, create NotificationLog.ts
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface INotificationLog extends Document {
+  habitId: string;
+  userEmail: string;
+  sentDate: string;
+}
+
+const notificationLogSchema = new Schema({
+  habitId: { type: String, required: true },
+  userEmail: { type: String, required: true },
+  sentDate: { type: String, required: true },
+});
+
+// Create a compound index for efficient lookups
+notificationLogSchema.index({ habitId: 1, userEmail: 1, sentDate: 1 }, { unique: true });
+
+const NotificationLog = mongoose.models.NotificationLog || 
+mongoose.model<INotificationLog>("NotificationLog", notificationLogSchema);
+
+export default NotificationLog;
