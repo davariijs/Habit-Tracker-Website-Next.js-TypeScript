e464785c518d08af73ff73fd34e1ec10f08e2b24
davariijs <Atuor127@gmail.com>
2025-03-07 07:28:42 -0500

    fix header

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
         <SidebarTrigger className='-ml-1' />
         <Separator orientation='vertical' className='mr-2 h-4' />
         <Breadcrumbs />
-        {/* <NotificationHandler/> */}
+        <NotificationHandler/>
       </div>
 
       <div className='flex items-center gap-2 px-4'>
